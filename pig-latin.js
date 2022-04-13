b90d5b5d27f95643c9ecc91e5737fdf917c96fbd
Lucas Matos <negocios.matos@gmail.com>
2022-04-13 00:23:29 -0300

    refactor: part 1

--- pig-latin.js
+++ pig-latin.js
@@ -1,62 +1,65 @@
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
+rl.question("What is the phrase that you want to translate?\n", translate);
 
+rl.on("close", function () {
+  process.exit(0);
+});
 
-const getInput = (input) => {
-    const lowerCaseVowels = ['a','e','i','o','u','y'];
-    const vowels = [...lowerCaseVowels, ...lowerCaseVowels.map((letter) => letter.toUpperCase())]
-
-    let phrase = input;
-    let output = '';
-
-    const separate = (whole) => {
-        return whole.split(' ');
+function findPrefixAndStem(word) {
+  const lowerCaseVowels = ["a", "e", "i", "o", "u", "y"];
+  const vowels = [
+    ...lowerCaseVowels,
+    ...lowerCaseVowels.map((letter) => letter.toUpperCase()),
+  ];
+  let prefix = "";
+  let stem = "";
+  let i = 0;
+  for (i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      break;
     }
-    
-    const findParts = (word) => {
-        let prefix = '';
-        let stem = '';
-        let i = 0;
-        for (i = 0; i < word.length; i++) {
-            if (vowels.includes(word[i])) {
-                break;
-            }
-            prefix += word[i];
-        }
-        for (; i < word.length; i++) {
-            stem += word[i];
-        }
-        return {prefix, stem};
-    }
-    const separated = separate(phrase);
-    for (let i = 0; i < separated.length; i++) {
-        const word = separated[i];
-        let {prefix, stem} = findParts(word);
-        stem = stem.toLowerCase();
-        let firstLetter = stem[0];
-        if (i === 0) {
-            firstLetter = firstLetter.toUpperCase()
-        } 
-        let oldStem = stem.substring(1, stem.length);
-        stem = firstLetter;
-        for (let i = 0; i < oldStem.length; i++) {
-            stem += oldStem[i];
-        }
-        
-        output += stem + prefix.toLowerCase() + 'ay' + ' ';
-    }
-    console.log(output);
-    rl.close();
+    prefix += word[i];
+  }
+  for (let j = i; j < word.length; j++) {
+    stem += word[j];
+  }
+  return { prefix, stem };
 }
 
-rl.question("What is the phrase that you want to translate?\n", getInput);
+function separate(whole) {
+  return whole.split(" ");
+}
 
+function fixLetterCase(stem, prefix, wordPosition) {
+  stem = stem.toLowerCase();
+  let firstLetter = stem[0];
+  if (wordPosition === 0) {
+    firstLetter = firstLetter.toUpperCase();
+  }
+  let oldStem = stem.substring(1, stem.length);
+  stem = firstLetter;
+  for (let i = 0; i < oldStem.length; i++) {
+    stem += oldStem[i];
+  }
 
+  return stem + prefix.toLowerCase();
+}
 
-rl.on("close", function() {
-    process.exit(0);
-});
+function translate(input) {
+  let phrase = input;
+  let output = "";
+
+  const separated = separate(phrase);
+  for (let i = 0; i < separated.length; i++) {
+    const word = separated[i];
+    let { prefix, stem } = findPrefixAndStem(word);
+    output += fixLetterCase(stem, prefix, i) + "ay" + " ";
+  }
+  console.log(output);
+  rl.close();
+}
